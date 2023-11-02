7f876e6edf9b76a4782ad7ed879951395e4f8e38
gudawuha <149662714+gudawuha@users.noreply.github.com>
2023-11-02 23:57:52 +0800

    Update pic.py

--- pic.py
+++ pic.py
@@ -20,15 +20,13 @@ session.viewports['Viewport: 1'].maximize()
 viewPort.setValues(displayedObject=odb)
 session.viewports['Viewport: 1'].view.setProjection(projection=PARALLEL)
 session.viewports['Viewport: 1'].view.setValues(session.views['Bottom'])
-session.pngOptions.setValues(imageSize=(4096,1792))
-session.printOptions.setValues(reduceColors=False)
-session.printToFile(fileName='C:\Users\Administrator\Desktop\damge3',format=PNG)
 RootPath = 'Post-' + mname[0:-4]
 RootPath = os.path.abspath(RootPath)
 if not os.path.exists(RootPath):
     os.makedirs(RootPath)
 os.chdir(RootPath) 
 for i in range(0,9):
+	
 	session.viewports['Viewport: 1'].odbDisplay.setPrimaryVariable(
 	variableLabel='DAMAGEC', outputPosition=INTEGRATION_POINT, )
 	session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
@@ -38,4 +36,34 @@ for i in range(0,9):
 	session.printOptions.setValues(rendition=COLOR,
 	vpDecorations=OFF, vpBackground=OFF)
 	session.pngOptions.setValues(imageSize=(4096, 4096))
-	session.printToFile(fileName="%s frame=%s, time=%s" % (mname, 8*i, i), format=PNG, canvasObjects=(viewPort,))
+	session.printToFile(fileName="DAMAGEC% sframe=%s, time=%s" % (mname[0:-4], 8*i, i), format=PNG, canvasObjects=(viewPort,))
+session.viewports['Viewport: 1'].view.setValues(session.views['Bottom'])
+for i in range(0,9):
+	leaf = dgo.LeafFromElementSets(elementSets=("BAR-1.SET_ALL_BAR", ))
+	session.viewports['Viewport: 1'].odbDisplay.displayGroup.replace(leaf=leaf)
+	session.viewports['Viewport: 1'].odbDisplay.setPrimaryVariable(
+	variableLabel='S', outputPosition=INTEGRATION_POINT, refinement=(INVARIANT, 
+	'Mises'), )
+	session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
+	CONTOURS_ON_DEF, ))
+	session.viewports[session.currentViewportName].odbDisplay.setFrame(
+	step='Step-2', frame=8*i)
+	session.printOptions.setValues(rendition=COLOR,
+	vpDecorations=OFF, vpBackground=OFF)
+	session.pngOptions.setValues(imageSize=(4096, 4096))
+	session.printToFile(fileName="S% sframe=%s, time=%s" % (mname[0:-4], 8*i, i), format=PNG, canvasObjects=(viewPort,))
+session.viewports['Viewport: 1'].view.setValues(session.views['Bottom'])
+for i in range(0,9):
+	leaf = dgo.LeafFromElementSets(elementSets=("BAR-1.SET_ALL_BAR", ))
+	session.viewports['Viewport: 1'].odbDisplay.displayGroup.replace(leaf=leaf)
+	session.viewports['Viewport: 1'].odbDisplay.setPrimaryVariable(
+	variableLabel='PE', outputPosition=INTEGRATION_POINT, refinement=(
+	INVARIANT, 'Max. In-Plane Principal'), )
+	session.viewports['Viewport: 1'].odbDisplay.display.setValues(plotState=(
+	CONTOURS_ON_DEF, ))
+	session.viewports[session.currentViewportName].odbDisplay.setFrame(
+	step='Step-2', frame=8*i)
+	session.printOptions.setValues(rendition=COLOR,
+	vpDecorations=OFF, vpBackground=OFF)
+	session.pngOptions.setValues(imageSize=(4096, 4096))
+	session.printToFile(fileName="PE% sframe=%s, time=%s" % (mname[0:-4], 8*i, i), format=PNG, canvasObjects=(viewPort,))
